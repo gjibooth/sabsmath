91fbdf8457f2996b454539a4270d0f1ff53609af
alisterde <alister.dale-evans@dtc.ox.ac.uk>
2020-02-07 12:04:29 +0000

    working stoch

--- customStochastic.py
+++ customStochastic.py
@@ -7,12 +7,12 @@ import numpy as np
 '''
 #defining initial conditions for species in concentration
 #TODO:these need to be convert to number of molecules
-C2 = np.array([0.015*(6.022*10**17)])
-CP = np.array([0.015*(6.022*10**17)])
-pM = np.array([0.001*(6.022*10**17)])
-M  = np.array([0.001*(6.022*10**17)])
-Y  = np.array([0.005*(6.022*10**17)])
-YP = np.array([0.005*(6.022*10**17)])
+C2 = [0.015*(6*10**3)]
+CP = [0.015*(6*10**3)]
+pM = [0.001*(6*10**3)]
+M  = [0.001*(6*10**3)]
+Y  = [0.005*(6*10**3)]
+YP = [0.005*(6*10**3)]
 
 YT = [Y[0] + YP[0] + pM[0] + M[0]]
 CT = [C2[0] + CP[0] + pM[0] + M[0]]
@@ -51,6 +51,8 @@ V_p = np.array([[1, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 1],
                 [0, 0, 0, 0, 1, 0]])
+#stoichiometry of total reaction
+V_tot =V_r-V_p
 # initial concentration
 X0 = np.array([Y[0], pM[0], M[0], YP[0], C2[0], CP[0]])
 
@@ -58,20 +60,17 @@ def react(propensity):
     '''Works out which reaction has occurred
     '''
     rand2 = np.random.rand()
-    print('rand2: ', rand2)
     num =0
     for i in propensity:
-        print('i: ', i)
         if rand2 <=  i:
             num = i 
-            print('num: ', num)
             break
         
     return propensity.index(num)
 
 # reaction rates
-time = np.array([0.0])
-while time[-1] < 0.5: 
+time = [0]
+while time[-1] < 1: 
     # rate of each reaction
     k = np.array([k1aaCT, k2, k3CT/(C2[-1] + CP[-1] +pM[-1] + M[-1]), 
     (k4prime+k4*((M[-1]/CT[-1])**2)), k5tilP, k6, k7, k8tilP, k9])
@@ -89,38 +88,56 @@ while time[-1] < 0.5:
     for prob in ProbReact:
         tot = tot + prob
         propensity.append(tot)
-    print('propensity: ', propensity)
      # Generate random numbers r1,r2 uniformly distributed in (0,1)
     rand1 = np.random.rand()
 
     # Compute the time until the next reaction takes place.
     tau = (1.0/ Rtot)*np.log(float(1.0/rand1))
-    np.append(time, (time[-1] +tau))
+    print('tau: ', tau)
+    time.append(time[-1] +tau)
+    print('time: ', time[-1])
 
     #working out which reaction has occured
     reaction = react(propensity)
-    print(reaction)
+    #print('reaction choosen: ', reaction)
 
     # array of current number of molecules
-    molc = np.array([Y[-1], pM[-1], M[-1], YP[-1], C2[-1], CP[-1]])
+    #molc = np.array([Y[-1], pM[-1], M[-1], YP[-1], C2[-1], CP[-1]])
 
-    molc = molc - V_r[reaction] + V_p[reaction]
+    #molc = molc - V_r[reaction] + V_p[reaction]
 
-    np.append(Y, molc[0])
-    np.append(pM, molc[1])
-    np.append(M, molc[2])
-    np.append(YP, molc[3])
-    np.append(C2, molc[4])
-    np.append(CP, molc[5])
+    molc = [Y[-1], pM[-1], M[-1], YP[-1], C2[-1], CP[-1]]
+    for i in range(0,len(molc),1):
+        molc[i]= molc[i] + V_tot[reaction][i]
+
+    Y.append(molc[0])
+    pM.append(molc[1])
+    M .append(molc[2])
+    YP.append(molc[3])
+    C2.append(molc[4])
+    CP.append(molc[5])
 
 plt.figure(figsize=(18,10))
 plt.title("MPF - SSA")
 plt.xlabel("Time (min)")
 plt.ylabel("Species Population")
-YT = Y + YP + pM + M
-CT = C2 + CP + pM + M
-plt.plot(time ,YT/CT,'r', label='[YT]/[CT]')
-plt.plot(time ,M/YT,'y', label='[M]/[YT]')
+Ynp = np.asarray(Y)
+YPnp = np.asarray(YP)
+pMnp = np.asarray(pM)
+Mnp = np.asarray(M)
+CPnp = np.asarray(CP)
+C2np = np.asarray(C2)
+YT = Ynp + YPnp + pMnp + Mnp
+#print('YT: ', YT)
+CT = C2np + CPnp + pMnp + Mnp
+#print('CT: ', CT)
+ytct = YT/CT
+myt = Mnp/YT
+timenp = np.asarray(time)
+#print('myt: ', myt)
+#print('ytct: ', ytct)
+plt.plot(time ,ytct,'r', label='[YT]/[CT]')
+plt.plot(time ,myt,'y', label='[M]/[YT]')
 
 plt.plot([0],[11])
 plt.legend(loc='best')
